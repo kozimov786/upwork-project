25bb5f8150c957b399f7c8dfe3dad1e8b712dcee
kozimov786 <email@youremail.com>
2022-02-16 18:37:43 +0500

    add areas of focus

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const btn = document.querySelector('.burger__btn')
 const Menu = document.querySelector('.nav');
+let navbar = document.querySelector('.navbar')
 
 btn.addEventListener('click', () => {
   Menu.classList.toggle('block')
 })
+
+window.onscroll = function () { myFunction() };
+let sticky = navbar.offsetTop;
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    console.log('lo');
+    navbar.classList.remove("sticky");
+  }
+}
